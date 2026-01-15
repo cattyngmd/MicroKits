c4cab40afeea286a247d001f1b4fd2a580495755
cattyn <cvenby@gmail.com>
2026-01-15 14:38:53 +0300

    feat: better item json serialization

--- src/main/java/dev/cattyn/microkits/utils/SerializationUtil.java
+++ src/main/java/dev/cattyn/microkits/utils/SerializationUtil.java
@@ -1,39 +1,23 @@
 package dev.cattyn.microkits.utils;
 
+import com.google.gson.JsonObject;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.io.BukkitObjectInputStream;
-import org.bukkit.util.io.BukkitObjectOutputStream;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.util.Map;
 
-public class SerializationUtil {
-    public static String serialize(ItemStack stack) throws IllegalStateException {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-
-            dataOutput.writeObject(stack);
-
-            dataOutput.close();
-            return Base64Coder.encodeLines(outputStream.toByteArray());
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to save item stacks.", e);
-        }
+public final class SerializationUtil {
+    private SerializationUtil() {
     }
 
-    public static ItemStack deserialize(String data) throws IOException {
-        try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
-            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-            ItemStack stack = (ItemStack) dataInput.readObject();
-            dataInput.close();
-            return stack;
-        } catch (ClassNotFoundException e) {
-            throw new IOException("Unable to decode class type.", e);
-        }
+    public static JsonObject serialize(ItemStack stack) {
+        return Globals.GSON.toJsonTree(stack.serialize()).getAsJsonObject();
     }
 
+    @SuppressWarnings("unchecked")
+    public static ItemStack deserialize(JsonObject data) {
+        if (data == null || data.isJsonNull())
+            return new ItemStack(Material.AIR);
+        return ItemStack.deserialize(Globals.GSON.fromJson(data, Map.class));
+    }
 }
