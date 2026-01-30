5c5105f2c82287bb22579ac6fd946dfcc89351fb
cattyn <cvenby@gmail.com>
2026-01-30 19:56:03 +0300

    fix: revert item serialization

--- src/main/java/dev/cattyn/microkits/utils/SerializationUtil.java
+++ src/main/java/dev/cattyn/microkits/utils/SerializationUtil.java
@@ -1,23 +1,43 @@
 package dev.cattyn.microkits.utils;
 
-import com.google.gson.JsonObject;
-import org.bukkit.Material;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
-import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public final class SerializationUtil {
     private SerializationUtil() {
     }
 
-    public static JsonObject serialize(ItemStack stack) {
-        return Globals.GSON.toJsonTree(stack.serialize()).getAsJsonObject();
+    public static JsonElement serialize(ItemStack stack) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            dataOutput.writeObject(stack);
+
+            dataOutput.close();
+            return new JsonPrimitive(Base64Coder.encodeLines(outputStream.toByteArray()));
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
     }
 
-    @SuppressWarnings("unchecked")
-    public static ItemStack deserialize(JsonObject data) {
-        if (data == null || data.isJsonNull())
-            return new ItemStack(Material.AIR);
-        return ItemStack.deserialize(Globals.GSON.fromJson(data, Map.class));
+    public static ItemStack deserialize(JsonElement data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data.getAsString()));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack stack = (ItemStack) dataInput.readObject();
+            dataInput.close();
+            return stack;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
     }
 }
